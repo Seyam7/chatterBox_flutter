18b41a5502ce874c7a58bd647d60f0c9d15c0d13
Sadman Seyam <sadman.seyam07@gmail.com>
2025-02-10 11:33:39 +0600

    app started with various color scheme

---
+++ android/app/src/main/kotlin/com/example/chatter_box_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chatter_box_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chatter_box_flutter/pages/login_page.dart';
+import 'package:chatter_box_flutter/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChatterBox',
+      home: LoginPage(),
+      theme: lightMode,
+    );
+  }
+}
