36b8d91c432383ae40df2a1bc66fa595118bf2aa
Sadman Seyam <sadman.seyam07@gmail.com>
2025-02-11 13:01:44 +0600

    firebase_core and firebase_auth import and setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:chatter_box_flutter/auth/login_or_register.dart';
+import 'package:chatter_box_flutter/firebase_options.dart';
 import 'package:chatter_box_flutter/pages/login_page.dart';
 import 'package:chatter_box_flutter/pages/register_page.dart';
 import 'package:chatter_box_flutter/themes/light_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
