48d393fdbc339af90699a90d94bb6e21058a1980
Sadman Seyam <sadman.seyam07@gmail.com>
2025-02-13 17:58:25 +0600

    logout button added in home page and sign out function completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,26 @@
+import 'package:chatter_box_flutter/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void logout(){
+    final _auth = AuthService();
+    _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
+        actions: [
+          // logout button
+          IconButton(
+              onPressed: logout,
+              icon: Icon(Icons.logout),
+          ),
+        ],
       ),
     );
   }
