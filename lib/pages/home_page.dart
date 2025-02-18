b9b6d3e3f92e9672b82584cf04ca5750c447794b
Sadman Seyam <sadman.seyam07@gmail.com>
2025-02-18 15:35:25 +0600

    cloud firestore installed, at home page all user will display and if we tap on user email than chat page will appear, but in home page users list is not showing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,16 @@
+import 'package:chatter_box_flutter/auth/auth_service.dart';
 import 'package:chatter_box_flutter/components/my_drawer.dart';
+import 'package:chatter_box_flutter/pages/chat_page.dart';
+import 'package:chatter_box_flutter/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
+import '../components/user_tile.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  // chat & auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +20,44 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: MyDrawer(),
+      body: _buildUserList(),
     );
+  }
+  // build a list of users except for the current logged user
+  Widget _buildUserList(){
+    return StreamBuilder(
+        stream: _chatService.getUsersStream(),
+        builder: (context,snapshot){
+          // error
+          if(snapshot.hasError){
+            return Text('Error');
+          }
+
+          // loading
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Text('Laoding');
+          }
+
+          // return list view
+          return ListView(
+            children: snapshot.data!.map<Widget>((userData) => _buildUserListItem(userData,context)).toList(),
+          );
+        },
+    );
+  }
+  Widget _buildUserListItem(Map<String,dynamic>userData, BuildContext context){
+    // display all user
+    return UserTile(
+      text: userData['email'],
+      onTap: (){
+        // tapped on a user -> go to chat page
+        Navigator.push(context,
+          MaterialPageRoute(
+            builder: (context)=>ChatPage(receiverEmail: userData['email'],)
+          ),
+        );
+      },
+    );
+
   }
 }
