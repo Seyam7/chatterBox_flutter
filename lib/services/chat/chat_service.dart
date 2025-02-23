91b8ba4a01e1ade1dd08c23ce66c3ec54df01165
Sadman Seyam <sadman.seyam07@gmail.com>
2025-02-23 10:42:24 +0600

    chat room created but text box is missing

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'package:chatter_box_flutter/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -33,18 +34,38 @@ class ChatService{
   }
 
   // send message
-Future<void> sendMessage(String receiverID, message)async{
-    // get current user info
-  final String currentUserID = _auth.currentUser!.uid;
-  final String? currentUserEmail = _auth.currentUser!.email;
-  final Timestamp timestamp = Timestamp.now();
+  Future<void> sendMessage(String receiverID, message)async{
+      // get current user info
+    final String currentUserID = _auth.currentUser!.uid;
+    final String? currentUserEmail = _auth.currentUser!.email;
+    final Timestamp timestamp = Timestamp.now();
 
-  // create a new message
+    // create a new message
+    Message newMassage = Message(
+        senderID: currentUserID,
+        senderEmail: currentUserEmail!,
+        receiverID: receiverID,
+        message: message,
+        timestamp: timestamp,
+    );
 
-  //construct a chat room Id for two users(sorted to ensure uniqueness)
+    //construct a chat room Id for two users(sorted to ensure uniqueness)
+    List<String> ids = [currentUserID, receiverID];
+    ids.sort(); // chat room is the same for any 2 peoples
+    String chatRoomID = ids.join('_');
 
-  // add new message to database
-}
+    // add new message to database
+    await _firestore.collection('chat_rooms').doc(chatRoomID).collection('messages').add(newMassage.toMap());
+  }
 
   // get message
+  Stream<QuerySnapshot> getMessages(String userID, otherUserID){
+    //construct a chatroom ID for the two users
+    List<String> ids = [userID, otherUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+    return _firestore.collection('chat_rooms').doc(chatRoomID).collection('messages').orderBy('timestamp',descending: false).snapshots();
+
+  }
+
 }
